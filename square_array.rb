ff1ae4c9c349d6cf2fe74d921ade5a87fec5aeb4
Justin Phillips <justinphil02@yahoo.com>
2019-10-25 18:12:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,6 @@
 def square_array(array)
  
-index = 0 
-new_array = array 
-
-array.each do |x|
-x = x * x 
-new_array[index] = x 
-index += 1 
+array.each do |num|
+num = num * num 
 end 
-
-return new_array
 end
